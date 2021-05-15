1bc724925d979994690a8cd877ca4d1440f92eb6
samuelFain <samuelfainberg@gmail.com>
2021-05-15 19:24:33 +0300

    switch to 'develop' branch, basic server code in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//set 'public' as static folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(port, () => {
+	console.log('Server started on port ' + port);
+});
