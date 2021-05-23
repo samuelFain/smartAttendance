43fb3a68f63e234cf52a0203d6eead55edfbf4a4
samuelFain <samuelfainberg@gmail.com>
2021-05-23 20:32:45 +0300

    webcam test page added, mongoDB charts integration in welcome page

---
+++ assets/demo.js
@@ -0,0 +1,7 @@
+let video = document.querySelector('#video');
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+		video.srcObject = stream;
+		video.play();
+	});
+}
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 
-
-
 // Welcome page | GET /
 router.get('/', (req, res) => {
 	res.render('welcome');
@@ -14,6 +12,12 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
 	res.render('dashboard', {
 		name: req.user.name,
 	});
+});
+
+// webcam demo page | Get /demo
+router.get('/demo', (req, res) => {
+	res.render('demo');
+	
 });
 
 module.exports = router;
